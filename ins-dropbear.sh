8019f6ae8f89b124d955690a039996dab777abed
free net <74614880+arismaramar@users.noreply.github.com>
2023-03-08 04:30:42 +0700

    Update ins-dropbear.sh

--- ins-dropbear.sh
+++ ins-dropbear.sh
@@ -4,7 +4,7 @@ apt -y purge dropbear
 sleep 0.5
 apt-get -y --purge remove dropbear
 sleep 0.5
-wget -O /etc/default/dropbear https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/dropbear
 apt -y install dropbear
+wget -O /etc/default/dropbear https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/dropbear
 sleep 0.5
 /etc/init.d/dropbear restart
