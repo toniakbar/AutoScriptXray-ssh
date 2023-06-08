a80b7c121b7aeb3c1fed89e12fcebcc89711daad
toniakbar <58284699+toniakbar@users.noreply.github.com>
2023-06-08 23:54:14 +0700

    Update ins-dropbear.sh

--- ins-dropbear.sh
+++ ins-dropbear.sh
@@ -5,6 +5,6 @@ sleep 0.5
 apt-get -y --purge remove dropbear
 sleep 0.5
 apt -y install dropbear
-wget -O /etc/default/dropbear https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/dropbear
+wget -O /etc/default/dropbear https://raw.githubusercontent.com/toniakbar/Xray/main/dropbear
 sleep 0.5
 /etc/init.d/dropbear restart
